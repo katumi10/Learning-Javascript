cdee0dc2632088d0b3f7e04e73c610ac71f2bf2f
katumi10 <143261500+katumi10@users.noreply.github.com>
2024-03-23 21:11:03 -0700

    arithmetic expressions

---
+++ expressions.js
@@ -0,0 +1,22 @@
+ // an arithmetic expression is a combinaion of operand  and operators
+ //to  produce a value
+ //operands are values, variables etc
+ //operators are +,- ,*,/ etc
+ 
+ let boys = 20;
+//  boys = boys + 1;  == boys += 1;//shortcut
+//  boys = boys - 1;  == boys -= 1;
+//  boys = boys * 1;  == boys *= 1;
+// boys = boys / 1;  == boys /= 1;
+
+extraBoys = boys % 3; //modulus operator
+
+ console.log(extraBoys);
+ console.log(boys);
+ /*
+ operator precedence
+ 1. parenthesis
+ 2. exponents
+ 3. multiplication and division
+ 4.addition and subtraction
+ */
