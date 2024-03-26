21a6262deb29f25b876e29fcaa540a344f4068a0
katumi10 <143261500+katumi10@users.noreply.github.com>
2024-03-26 10:02:08 -0700

    changing  data types

---
+++ typeconversion.js
@@ -0,0 +1,30 @@
+//type conversion = change the data type of a value to another
+//(strings,numbers,booleans)
+
+
+
+//converting string to a number
+
+// let age= window.prompt("What's your age");
+//  age = Number(age);
+// age += 1;
+// console.log(age);
+
+// console.log(typeof age);
+
+let a;
+let b;
+let c;
+
+a = Number("3.14")//String to number
+b = String(200)  //Number to string
+c = Boolean("food"); // to Boolean :  a non empty string is always true
+d = Boolean(""); // an empty string will result in false
+
+
+console.log(a,typeof a);
+console.log(b,typeof b);
+console.log(c,typeof c);
+console.log(d,typeof d);
+
+
