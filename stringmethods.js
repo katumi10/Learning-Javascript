9be460d23146dd7accab47452efad1d666ff789b
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-04-06 13:19:46 -0700

    updated string methods

--- stringmethods.js
+++ stringmethods.js
@@ -1,15 +1,29 @@
-// Javascript strimg methods
+// Javascript string methods
 
 
 let myName = "Katumi Amadu";
+let phone = "023-4784-6156";
 
-// let length = myName.length;To fing the length of a string
+console.log(myName)
+
+console.log(myName.length); //length of  a string
+console.log(myName.charAt(2)) // returns the character at a specified index(position)
+console.log(myName.indexOf("i"))// returns the index of the first occurence of a character
+console.log(myName.lastIndexOf("a"))// last occurence of  a string
+console.log(myName.toUpperCase()) 
+console.log(myName.toLowerCase())
+
+console.log(phone)
+console.log(phone.replaceAll("-","/")) //replaces - with /
+
+console.log(myName.concat(" ",phone)) //joins two strings together
+
+console.log(myName.includes("y")) // returns true if a string contains a specific character
+
+console.log(myName.slice(2,7)) // extracts a part of a string and returns the extracted part
 
 
 
 
-
-
-console.log(length)
 
 
