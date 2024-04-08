8538534e8b4bcd8b4d002c90bf6925ad08bfd2e5
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-04-08 07:19:27 -0700

    if statement

---
+++ else.js
@@ -0,0 +1,22 @@
+/* else statement is used to specify a block of code 
+to be executed if condition is false
+
+syntax:
+if(condition){
+    code to be executed if condition is true
+}  
+else{
+    code to be executed if condition is false
+}
+
+*/
+
+let time = 3.00;
+
+if(time>12.00){
+    greet = "Good day"
+}
+else{
+    greet = "Good Morning"
+}
+console.log(greet);
---
+++ if.js
@@ -0,0 +1,14 @@
+/* If statement is used to specify a block of code to 
+be executed if a condition is true  */
+
+/*syntax: 
+if(condition){
+          code to be executed if condition is true}
+ */
+
+          let time= 6.00
+
+          if(time < 12.00){
+            greet = "Good Morning"
+          }
+          console.log(greet);
