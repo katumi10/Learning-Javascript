d559fa23bbf028a8f1b015745c9cc315de4dfc63
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-04-16 10:24:25 -0700

    while loop,do while loop, for loop

---
+++ loops.js
@@ -0,0 +1,15 @@
+//while loop executes a block of code a long as
+//    a specified condition is true 
+/* 
+   while(condition){
+      code to be executed
+   }       
+*/
+ 
+/* do while loop: This will execute the code block once before 
+  checking if the condition is true, then it will repeat the loop
+  as long as the condition is true  
+  
+      do{code to be executed}
+        while(condition);
+  */
