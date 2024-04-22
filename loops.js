998907d1d3305eab3e0a3327bb9e5ef0e157ce45
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-04-22 04:21:10 -0700

    updated loops

--- loops.js
+++ loops.js
@@ -5,6 +5,15 @@
       code to be executed
    }       
 */
+
+let text= " ";
+let i = 0;
+
+// while(i<10){
+//    text += "<br>The number is" + " " + i;
+//    i++;
+   // document.getElementById("demo").innerHTML = text;
+// }
  
 /* do while loop: This will execute the code block once before 
   checking if the condition is true, then it will repeat the loop
@@ -13,3 +22,23 @@
       do{code to be executed}
         while(condition);
   */
+// do{text += "<br>The number is" + i;
+//         i++;}
+// while(i<10);
+// document.getElementById("demo").innerHTML = text;
+
+/* for loop: creates a loop with three optional expressions
+    for(expression1;expression2;expression3){
+      code to be executed
+   expresion1 : executed 1 time before the execution of the code.
+   
+   expression2 : defines the condition for executing the code.
+
+   expression3 : executed (everytime) after the cde has been executed
+   
+   }*/
+
+   // for (let i = 0; i<5; i++){
+   //    text+= "<br>The number is" + " "+ i;
+   // }
+   // document.getElementById("demo").innerHTML= text; 
