fb878a7030e3635e3934a09350128d50fa79ac65
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-05-09 17:07:23 +0000

    updated

---
+++ breakandcontinue.js
@@ -0,0 +1,12 @@
+// the break statement = apart from the switch statement,the break statement can be used to jump out of a loop.
+//it breaks out of a loop entirely
+
+
+for(let i =1; i<= 20; i+=1){
+    if(i===13){
+        break;//stops at 13
+      //  continue;//skips 13
+    }
+    console.log(i)
+}
+//the continue statement skips an iteration if a specified condition occurs and continues with the next iteration in the loop
