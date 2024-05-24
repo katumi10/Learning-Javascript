ad6f64e8e611ea59235fb63bc1f9ce79a3819ca9
katumi.codes <143261500+katumi10@users.noreply.github.com>
2024-05-24 16:43:34 +0000

    Arrays

---
+++ ARRAYS/script.js
@@ -0,0 +1,38 @@
+
+//array = like a variable that can store multiple values
+
+//You can access an array element by referrring to the index number
+const cars = ["BMW","Mercedes","Tundra","Toyota"];
+  let car = cars[1];
+  console.log(car); //output is Mercedes
+
+  //Changing an array element
+  const fruits = ["Apple","banana","orange","pawpaw"];
+        fruits[0] = "mango";
+        console.log(fruits); //output is Mango,banana,orange,pawpaw
+
+        //Adding an array element(.push method)
+        fruits.push("guava");
+        console.log(fruits);// Mango,banana,orange,pawpaw,guava
+
+        //Or using .length method
+        fruits[fruits.length] = "lemon";
+        console.log(fruits)//Mango,banana,orange,pawpaw,guava,lemon
+
+    //removing last element of an array
+    fruits.pop();
+    console.log(fruits)//Mango,banana,orange,pawpaw,guava
+
+    //adding an element at the beginning of an array
+    fruits.unshift("watermelon");
+
+    //removing an element at the beginning of an array
+    fruits.shift();
+
+    //the lenght of ana array
+    let length = fruits.length;
+    console.log(length);  //displays 5
+
+    //finding the index of an element in an array
+    let index = fruits.indexOf("banana");
+    console.log(index) //displays 1
---
+++ ARRAYS/sorting arrays/strings.js
@@ -0,0 +1,10 @@
+
+ //sorting an array of strings
+// sort() sorts an array alphabetically
+// reverse() reverses the element in an array in descending order
+let fruits = ["mango","banana","apple","coconut"];
+
+// fruits = fruits.sort();
+fruits = fruits.reverse();
+
+console.log(fruits)
